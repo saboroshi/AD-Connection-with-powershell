3e92d85f6d1d16620ff139d4845a536309130628
saboroshi <38815641+saboroshi@users.noreply.github.com>
2024-03-18 13:05:21 +0100

    AD-Connection.ps1

--- AD-Connection.ps1
+++ AD-Connection.ps1
@@ -1,3 +1,27 @@
+﻿<#	
+  .Synopsis
+   Connection test to Active Directory server..
+
+  .Description
+   Used to test Active Directory authentication. The script checks that this user with the specified username and password exists in AD. 
+   If the username and password pair exists, it returns true, otherwise it returns false. Installation of the RSAT client required for its operation:
+   
+  .Parameter Server
+   The Active Directory server.
+
+  .Parameter Domain
+   Domain name.
+
+  .Parameter Username
+   Domain user name.
+
+  .Parameter Password
+   Domain user password.
+
+  .Example
+   AD-Connection -Server "example.domain.com" -Domain "domain.com" -Username "aduser" -Password "adpassword"
+#>
+
 function AD-Connection
 {
     param(
